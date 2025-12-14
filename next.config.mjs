a0a66980960782ce89d625cbecc4814d63712823
populinlou-dot <populinlou@gmail.com>
2025-12-13 23:05:24 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
+  output: "export",
+  trailingSlash: true,
 
-export default nextConfig
+  // IMPORTANTE para "project pages":
+  // tu sitio va a vivir en /Bylou-landing/
+  basePath: "/Bylou-landing",
+  assetPrefix: "/Bylou-landing/",
+
+  images: {
+    unoptimized: true, // GitHub Pages no soporta el optimizador de next/image
+  },
+};
+
+export default nextConfig;
